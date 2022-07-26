0112248e6d060e2af2d797bc01ec469298246f65
Aakanksha Dhurandhar <aakankshadhurandhar34@gmail.com>
2022-07-26 19:07:57 +0530

    Time: 81 ms (58.34%), Space: 42.2 MB (36.37%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    const output = [];
+    
+    for (let i = 0; i < numRows; i++) {
+        const rowValue = [];
+        
+        for (let j = 0; j < i + 1; j++) {
+             if (j === 0 || j === i) {
+                 rowValue[j] = 1;
+             } else {
+                 rowValue[j] = output[i - 1][j - 1] + output[i - 1][j];
+             }
+        }
+        output.push(rowValue)
+    }
+    
+    return output;
+};
