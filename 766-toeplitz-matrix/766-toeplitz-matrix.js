20478bb40a055b8fa530bcd40333cf1f9fff8646
Aakanksha Dhurandhar <aakankshadhurandhar34@gmail.com>
2022-08-25 23:15:29 +0530

    Time: 104 ms (46.06%), Space: 44.1 MB (59.48%) - LeetHub

---
+++ 766-toeplitz-matrix/766-toeplitz-matrix.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var isToeplitzMatrix = function(matrix) {
+    var m=matrix.length
+    var n=matrix[0].length
+    
+    for(var i=0;i<m;i++){
+        for(var j=0;j<n;j++){
+            if(i>0 && j>0 && matrix[i-1][j-1] !=matrix[i][j]){
+                return false
+            }
+        }
+    }
+    return true
+};
